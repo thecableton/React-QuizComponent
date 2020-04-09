01e90fb14a3ff6c148bb2d18babaf7ec19bbcd91
Cableton <thecableton@gmail.com>
2020-04-09 12:31:22 +0100

    Woops, forgot to save QuizEnd

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -4,7 +4,7 @@ class QuizEnd extends Component  {
     render() {
         return (
             <div>
-                <p>Thanks for Playing!</p>
+                <p>Thanks for playing!</p>
                 <a href=''>Reset Quiz</a>
             </div>
         );
