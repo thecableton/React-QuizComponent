ee2f8abab4da16ff4ab7dd33e1b5394777d704be
Cableton <thecableton@gmail.com>
2020-04-09 12:43:00 +0100

    standards changes to QuizEnd.js

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class QuizEnd extends Component  {
+class QuizEnd extends Component {
     render() {
         return (
             <div>
