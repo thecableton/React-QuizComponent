2fc0f46a3f244675b23ca1566347e472b15ab8e0
Cableton <thecableton@gmail.com>
2020-04-09 15:00:38 +0100

    Added resetClickHandler property to Quiz.js JSX

--- src/Quiz.js
+++ src/Quiz.js
@@ -25,7 +25,7 @@ class Quiz extends React.Component {
             const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
             return (
                 <div>
-                    {isQuizEnd ? <QuizEnd /> :
+                    {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
                 </div>
             );
