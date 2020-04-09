bb0e249fb9bd9b9207915c21bef21c0cb6201f61
Cableton <thecableton@gmail.com>
2020-04-09 14:23:40 +0100

    Corrected standards error in QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,14 +3,12 @@ import React, { Component } from "react";
 class QuizQuestionButton extends Component {
 
     handleClick(){
-        this.props.clickHandler(this.props.button_text);
+        this.props.clickHandler(button_text);
     };
     render() {
         return (
             <li>
-                <button onclick={this.handleClick.bind(this)}>
-                    {this.props.button_text}
-                </button>
+                <button onclick={this.handleClick.bind(this)}>{this.props.button_text}</button>
             </li>
         );
     };
