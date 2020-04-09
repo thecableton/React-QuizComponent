a934375a8f66b7a496069a8c32741713cc5aeb07
Cableton <thecableton@gmail.com>
2020-04-09 12:19:35 +0100

    implemented QuizEnd.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -10,8 +11,11 @@ class Quiz extends React.Component {
     };
 
     render() {
+        const isQuizEnd = quizData.quiz_questions.length;
         return (
         <div>
+            if (isQuizEnd == -1)
+            <QuizEnd />
             <QuizQuestion  quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
         </div>
         )};
---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class QuizEnd extends Component  {
+    render() {
+        return (
+            <div>
+                <p>Thanks for Playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        );
+    };
+}
+
+export default QuizEnd;
