f0eaa09702f504b9e654fafca5430c2330c0bbf5
Cableton <thecableton@gmail.com>
2020-04-09 11:50:23 +0100

    additional corrections to exporting components

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,7 @@ import QuizQuestion from './QuizQuestion.js';
 
 let quizData = require('./quiz_data.json');
 
-export default class Quiz extends React.Component {
+class Quiz extends React.Component {
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 }
